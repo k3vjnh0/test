6b96dffecad5cf582a4e0988a44d030aded7c445
Nguyen Ho <duynguyen272@gmail.com>
2021-07-24 14:38:47 +0700

    fixed code

--- hello.py
+++ hello.py
@@ -2,10 +2,4 @@
 import myname
 
 name = myname.get_name()
-<<<<<<< HEAD
-# print("hello {}".format(name))
 print(f'hello {name}')
-=======
-print("hello {}".format(name))
-print("keep changing...")
->>>>>>> my_new_feature
